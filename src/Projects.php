f4fd4bc76cd23d29df1c0c25ee71e6cd55f8fb63
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:38 -0500

    doc/gen Updated `src/Projects.php`.

--- src/Projects.php
+++ src/Projects.php
@@ -5,17 +5,24 @@ namespace Zerotoprod\DataModelEnvoyer;
 use Zerotoprod\DataModel\Describe;
 use Zerotoprod\DataModelHelper\DataModelHelper;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class Projects
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#get-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const projects = 'projects';
 
     /**
      * @var Project[] $projects
      *
-     * @link https://envoyer.io/api-documentation#get-project
+     * @see  https://envoyer.io/api-documentation#get-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
      */
     #[Describe([
             'cast' => [DataModelHelper::class, 'mapOf'],
