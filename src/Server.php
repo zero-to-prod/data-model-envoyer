082b112b96e1d6f0f3c2c73dae82af7a1a01e268
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:38 -0500

    doc/gen Updated `src/Server.php`.

--- src/Server.php
+++ src/Server.php
@@ -2,89 +2,212 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class Server
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const id = 'id';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const project_id = 'project_id';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const user_id = 'user_id';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const name = 'name';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const connect_as = 'connect_as';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const ip_address = 'ip_address';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const port = 'port';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const php_seven = 'php_seven';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const php_version = 'php_version';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const freebsd = 'freebsd';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const receives_code_deployments = 'receives_code_deployments';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const should_restart_fpm = 'should_restart_fpm';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const deployment_path = 'deployment_path';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const php_path = 'php_path';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const composer_path = 'composer_path';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const public_key = 'public_key';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const connection_status = 'connection_status';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const current_activity = 'current_activity';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const created_at = 'created_at';
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const updated_at = 'updated_at';
 
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $id;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $project_id;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $user_id;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $name;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $connect_as;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $ip_address;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $port;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $php_seven;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public PhpVersion $php_version;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $freebsd;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $receives_code_deployments;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $should_restart_fpm;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $deployment_path;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $php_path;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $composer_path;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $public_key;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $connection_status;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public ?string $current_activity;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $created_at;
-    /** @link https://envoyer.io/api-documentation#get-server */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-server
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $updated_at;
 }
