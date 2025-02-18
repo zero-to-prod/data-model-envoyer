a22e3deb30139a423539bd16c1776009893cac66
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:37 -0500

    doc/gen Updated `src/NotificationType.php`.

--- src/NotificationType.php
+++ src/NotificationType.php
@@ -2,10 +2,25 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 enum NotificationType: string
 {
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case email = 'email';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case discord = 'discord';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case slack = 'slack';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case teams = 'teams';
 }
