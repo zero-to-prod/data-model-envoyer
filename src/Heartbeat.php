cd404cc392bf2d9d55e42787bb12a11684d826fe
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:37 -0500

    doc/gen Updated `src/Heartbeat.php`.

--- src/Heartbeat.php
+++ src/Heartbeat.php
@@ -2,45 +2,102 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class Heartbeat
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const id = 'id';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const project_id = 'project_id';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const name = 'name';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const token = 'token';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const interval = 'interval';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const status = 'status';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const last_checked_in_at = 'last_checked_in_at';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const created_at = 'created_at';
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const updated_at = 'updated_at';
 
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $id;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $project_id;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $name;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $token;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $interval;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $status;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $last_checked_in_at;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $created_at;
-    /** @link https://envoyer.io/api-documentation#get-heartbeat */
+    /**
+     * @see  https://envoyer.io/api-documentation#get-heartbeat
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $updated_at;
 }
