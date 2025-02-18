eca15c92e3327519155670e9ba9802d22f718619
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:37 -0500

    doc/gen Updated `src/ProjectType.php`.

--- src/ProjectType.php
+++ src/ProjectType.php
@@ -2,17 +2,27 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 enum ProjectType: string
 {
 
-    /** @link https://envoyer.io/api-documentation#create-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#create-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case laravel_5 = 'laravel-5';
-    /** @link https://envoyer.io/api-documentation#create-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#create-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case laravel_4 = 'laravel-4';
     /**
      * Static HTML, Other PHP Projects
      *
-     * @link https://envoyer.io/api-documentation#create-project
+     * @see  https://envoyer.io/api-documentation#create-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
      */
     case other = 'other';
 }
