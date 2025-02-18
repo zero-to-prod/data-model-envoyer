0fd8c5dd074817560e29c3ca88223239f7b3f093
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:36 -0500

    doc/gen Updated `src/Actions.php`.

--- src/Actions.php
+++ src/Actions.php
@@ -5,17 +5,24 @@ namespace Zerotoprod\DataModelEnvoyer;
 use Zerotoprod\DataModel\Describe;
 use Zerotoprod\DataModelHelper\DataModelHelper;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class Actions
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#list-actions */
+    /**
+     * @see  https://envoyer.io/api-documentation#list-actions
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const actions = 'actions';
 
     /**
      * @var Action[] $actions
      *
-     * @link https://envoyer.io/api-documentation#list-actions
+     * @see  https://envoyer.io/api-documentation#list-actions
+     * @link https://github.com/zero-to-prod/data-model-envoyer
      */
     #[Describe([
             'cast' => [DataModelHelper::class, 'mapOf'],
