c91cc7104be28c797f6422cc3c3d050349733ac7
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:36 -0500

    doc/gen Updated `src/CreateCollaborator.php`.

--- src/CreateCollaborator.php
+++ src/CreateCollaborator.php
@@ -2,13 +2,22 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class CreateCollaborator
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#create-collaborator */
+    /**
+     * @see  https://envoyer.io/api-documentation#create-collaborator
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const email = 'email';
 
-    /** @link https://envoyer.io/api-documentation#create-collaborator */
+    /**
+     * @see  https://envoyer.io/api-documentation#create-collaborator
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $actions;
 }
