201282fc5973318da1c44bcf7d972e7acb81044f
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:38 -0500

    doc/gen Updated `src/UpdateProject.php`.

--- src/UpdateProject.php
+++ src/UpdateProject.php
@@ -2,33 +2,72 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 class UpdateProject
 {
     use DataModel;
 
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const name = 'name';
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const retain_deployments = 'retain_deployments';
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const monitor = 'monitor';
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const composer = 'composer';
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const composer_dev = 'composer_dev';
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public const composer_quiet = 'composer_quiet';
 
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $name;
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public int $retain_deployments;
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public string $monitor;
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $composer;
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $composer_dev;
-    /** @link https://envoyer.io/api-documentation#update-project */
+    /**
+     * @see  https://envoyer.io/api-documentation#update-project
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     public bool $composer_quiet;
 }
