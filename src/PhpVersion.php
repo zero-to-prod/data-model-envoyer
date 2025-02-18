e93aabf4f1b9d1b7794b78cd531f57072e264e70
David Smith <dasmith@zero-to-prod.com>
2025-02-17 21:22:37 -0500

    doc/gen Updated `src/PhpVersion.php`.

--- src/PhpVersion.php
+++ src/PhpVersion.php
@@ -2,17 +2,53 @@
 
 namespace Zerotoprod\DataModelEnvoyer;
 
+/**
+ * @link https://github.com/zero-to-prod/data-model-envoyer
+ */
 enum PhpVersion: string
 {
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php84 = 'php84';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php83 = 'php83';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php82 = 'php82';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php81 = 'php81';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php80 = 'php80';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php74 = 'php74';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php73 = 'php73';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php72 = 'php72';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php71 = 'php71';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php70 = 'php70';
+    /**
+     * @link https://github.com/zero-to-prod/data-model-envoyer
+     */
     case php56 = 'php56';
 }
